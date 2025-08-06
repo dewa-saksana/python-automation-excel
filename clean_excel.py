ede20a57ec27dece3d1d396bbd012256e56412bd
jidancode <146992196+dewa-saksana@users.noreply.github.com>
2025-08-06 21:41:21 +0700

    Create clean_excel.py

---
+++ clean_excel.py
@@ -0,0 +1,14 @@
+# clean_excel.py
+import openpyxl
+
+wb = openpyxel.load_workbook(data.xlsx)
+ws = nwb.active
+
+# Delete empty line
+for row in range(ws.max_row, 0, -1):
+  if all([cell.value is None for cell in ws[row]]):
+    ws.delete_rows(row)
+
+wb.save("data_cleaned.xlsx")
+print("Cleaning complete."}
+  
